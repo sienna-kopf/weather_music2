a458e64cc7683871a91bf65ddaeb43017d767622
Sienna Kopf <princess.kopf@gmail.com>
2020-09-11 18:24:03 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'rake'
+gem 'require_all'
+gem 'thin'
+gem 'shotgun'
+gem 'pry'
+gem 'bcrypt'
+gem 'tux'
+
+group :test do
+  gem 'rspec'
+  gem 'capybara'
+  gem 'rack-test'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+ENV["SINATRA_ENV"] = "test"
+
+require_relative '../config/environment'
+require 'rack/test'
+require 'capybara/rspec'
+require 'capybara/dsl'
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.include Rack::Test::Methods
+  config.include Capybara::DSL
+
+  config.order = 'default'
+end
+
+def app
+  Rack::Builder.parse_file('config.ru').first
+end
+
+Capybara.app = app
