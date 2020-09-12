3501bfe7cfed2906094abc6313387e0563d32596
Josh Tukman <josh.tukman@gmail.com>
2020-09-12 14:06:36 -0600

    Add Spotify Service request with necessary arguments

--- app/controllers/weather_music_controller.rb
+++ app/controllers/weather_music_controller.rb
@@ -8,10 +8,16 @@ class WeatherMusicController < ApplicationController
   end
 
   get "/weather_playlist" do
+    access_token = params[:access_token]
+    refresh_token = params[:refresh_token]
     location = params[:q]
     weather_response = WeatherService.new.forecast(location)
+    binding.pry
     if weather_response[:cod] == 200
       forecast = Forecast.new(weather_response)
+      weather = forecast.description
+      spotify_response = SpotifyService.new.playlist(access_token, refresh_token, weather)
+      binding.pry
       WeatherMusicSerializer.new(forecast).data_hash.to_json
     else
       WeatherMusicSerializer.new.no_response.to_json
