fa67693fd150dd83e9576587086dbea15978d729
Sienna Kopf <princess.kopf@gmail.com>
2020-09-12 20:31:35 -0600

    Add conditional logic and spotify service pieces to the controller

--- app/controllers/weather_music_controller.rb
+++ app/controllers/weather_music_controller.rb
@@ -13,13 +13,13 @@ class WeatherMusicController < ApplicationController
     weather_response = WeatherService.new.forecast(location)
     if weather_response[:cod] == 200
       forecast = Forecast.new(weather_response)
-      spotify_response = SpotifyService.new.playlist(token, forecast.description)
-      if spotify_response[:cod] == 200
+      spotify_response = SpotifyService.new.playlist(token, forecast.main_description)
+      unless spotify_response[:playlists][:items].empty?
         playlist = Playlist.new(spotify_response)
         WeatherMusicSerializer.new(forecast, playlist).data_hash.to_json
       else
         WeatherMusicSerializer.new.no_playlist_response.to_json
-      end 
+      end
     else
       WeatherMusicSerializer.new.no_city_response.to_json
     end
