4b229597cca1369294b4e10d9622aff377b3155d
Sienna Kopf <princess.kopf@gmail.com>
2020-09-12 19:56:31 -0600

    JSON API 1.0 format fix

--- app/serializers/weather_music_serializer.rb
+++ app/serializers/weather_music_serializer.rb
@@ -7,34 +7,56 @@ class WeatherMusicSerializer
 
   def data_hash
     {
-       city_name: @forecast.city_name,
-       country_name: @forecast.country_name,
-       sunrise_time: @forecast.sunrise_time,
-       sunset_time: @forecast.sunset_time,
-       description: @forecast.description,
-       temp: @forecast.temp,
-       temp_min: @forecast.temp_min,
-       temp_max: @forecast.temp_max,
-       pressure: @forecast.pressure,
-       humidity: @forecast.humidity,
-       visibility: @forecast.visibility,
-       wind: @forecast.wind,
-       playlist_id: @playlist.id,
-       playlist_uri: @playlist.uri
+      "data": {
+        "weather": {
+          "type": "forecast",
+          "attributes": {
+            "city_name": @forecast.city_name,
+            "country_name": @forecast.country_name,
+            "sunrise_time": @forecast.sunrise_time,
+            "sunset_time": @forecast.sunset_time,
+            "description": @forecast.description,
+            "temp": @forecast.temp,
+            "temp_min": @forecast.temp_min,
+            "temp_max": @forecast.temp_max,
+            "pressure": @forecast.pressure,
+            "humidity": @forecast.humidity,
+            "visibility": @forecast.visibility,
+            "wind": @forecast.wind
+          }
+        },
+        "music": {
+          "type": "playlist",
+          "attributes": {
+            "id": @playlist.id,
+            "uri": @playlist.uri
+          }
+        }
       }
+    }
   end
 
   def no_city_response
     {
-      code: 404,
-      message: "city_not_found"
+      "data": {
+        "type": "error",
+        "attributes": {
+          "code": 404,
+          "message": "city_not_found"
+        }
+      }
     }
   end
 
   def no_playlist_response
     {
-      code: 404,
-      message: "play_list_not_found"
+      "data": {
+        "type": "error",
+        "attributes": {
+          "code": 404,
+          "message": "play_list_not_found"
+        }
+      }
     }
   end
 end
