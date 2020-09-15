7934a41722d90e238c59d008f2839648e58c54d6
Sienna Kopf <princess.kopf@gmail.com>
2020-09-15 11:12:59 -0600

    Configure service with new endpoint to handle audio correlation

--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -7,22 +7,26 @@ class SpotifyService
     to_json("?q=#{weather}&type=playlist&limit=20&offset=5", token)
   end
 
+  def create_playlist(target_valence, target_speech, target_mode, target_energy, target_tempo, seed_tracks, token)
+    to_json_rec("?seed_tracks=#{seed_tracks}&target_valence=#{target_valence}&target_speechiness=#{target_speech}&target_energy=#{target_energy}&target_mode=#{target_mode}&target_tempo=#{target_tempo}", token)
+  end
+
   def weather_tracks_first_50(token)
     to_json_tracks("?offset=1&limit=50", token)
   end
 
-  def weather_tracks_second_50(token)
-    to_json_tracks("?offset=50&limit=50", token)
-  end
+  # def weather_tracks_second_50(token)
+  #   to_json_tracks("?offset=50&limit=50", token)
+  # end
+  #
+  # def audio_features(uris, token)
+  #   to_json_features("?ids=#{uris}", token)
+  # end
 
-  def audio_features(uris, token)
-    to_json_features("?ids=#{uris}", token)
-  end
-
-  def create_playlist(tracks, token)
-    to_json_features("?ids=#{uris}", token)
-
-  end
+  # def create_playlist(tracks, token)
+  #   to_json_features("?ids=#{uris}", token)
+  #
+  # end
 
   private
 
@@ -31,6 +35,18 @@ class SpotifyService
     Authorization: "Bearer #{token}"
     }
     acc = Faraday.new(url: 'https://api.spotify.com/v1/search', headers: header)
+  end
+
+  def conn_rec(token)
+    header = {
+    Authorization: "Bearer #{token}"
+    }
+    acc = Faraday.new(url: 'https://api.spotify.com/v1/recommendations', headers: header)
+  end
+
+  def to_json_rec(url, token)
+    response = conn_rec(token).get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn_library(token)
