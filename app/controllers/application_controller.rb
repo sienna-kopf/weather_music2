e1a70b488427657920d65e967ac7ebe814af15c9
“Neeru <neeram85@gmail.com>
2020-09-14 18:42:00 -0600

    Testing getting audio features from spotify for tracks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
 
   def serialization(weather_response, token)
     if weather_success?(weather_response)
+      binding.pry
       forecast = Forecast.new(weather_response)
       spotify_response = SpotifyService.new.playlist(token, forecast.main_description)
       playlist = spotify_response[:playlists][:items].shuffle.first
@@ -32,5 +33,16 @@ class ApplicationController < Sinatra::Base
 
   def weather_success?(weather_response)
     weather_response[:cod] == 200
+  end
+
+  def test
+    # total_tracks = response_1[:total]
+    response_1 = SpotifyService.new.weather_tracks_first_50(token)
+    response_2 = SpotifyService.new.weather_tracks_second_50(token)
+    response_1_ids = response_1[:items].map {|item| item[:track][:id]}
+    response_2_ids= response_2[:items].map {|item| item[:track][:id]}
+    all_ids = response_1_ids+ response_2_ids
+    query = all_ids.join(",")
+    audio_features_data = SpotifyService.new.audio_features(query, token)
   end
 end
--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -7,6 +7,19 @@ class SpotifyService
     to_json("?q=#{weather}&type=playlist&limit=20&offset=5", token)
   end
 
+  def weather_tracks_first_50(token)
+    to_json_tracks("?offset=1&limit=50", token)
+  end
+
+  def weather_tracks_second_50(token)
+    to_json_tracks("?offset=50&limit=50", token)
+  end
+
+  def audio_features(uris, token)
+    binding.pry
+    to_json_features("?#{uris}", token)
+  end
+
   private
 
   def conn(token)
@@ -16,8 +29,32 @@ class SpotifyService
     acc = Faraday.new(url: 'https://api.spotify.com/v1/search', headers: header)
   end
 
+  def conn_library(token)
+    header = {
+    Authorization: "Bearer #{token}"
+    }
+    acc = Faraday.new(url: 'https://api.spotify.com/v1/me/tracks', headers: header)
+  end
+
+  def conn_features(token)
+    header = {
+    Authorization: "Bearer #{token}"
+    }
+    acc = Faraday.new(url: 'https://api.spotify.com/v1/audio-features', headers: header)
+  end
+
   def to_json(url, token)
     response = conn(token).get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def to_json_tracks(url, token)
+    response = conn_library(token).get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def to_json_features(url, token)
+    response = conn_features(token).get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
