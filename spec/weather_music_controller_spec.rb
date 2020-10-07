d6584c8c5e24c2016ef16a681b8f674d32c9cb7e
“Neeru <neeram85@gmail.com>
2020-10-07 12:12:25 -0600

    Uncommitted changes

--- spec/weather_music_controller_spec.rb
+++ spec/weather_music_controller_spec.rb
@@ -6,13 +6,11 @@ end
 
 describe WeatherMusicController do
   before :each do
-    @token = "BQDsKNYViB8eif9EaYLOxfU6D3P54qIuyh6TUdJAP9asvGsUeHZjil5oWg_8WCdDfhT8Ue6nfHVPshD9ft2pRCzO25ZyyR2uvHmf3vSq2yE5gxkDaNoAqBmora8kFXXXmK_XsHUsIZEZthy6-3T6oHVOFafWG6aP1YCIKplJdwFRN1wxvjsD3H3Wl73lI0U4-0j7A4803fjMs8WsWSgDqBuJvOX2ofbpQP755U_dfIJpNtg"
-    @user_id = "bosigp0djzqxoyj6yq6sdzzaq"
+    @token = "BQDdAPDPDGyotHHjwslG4XY-nJ59bLCmKfYp2SVo6jUz_ItI2lbPGdncNrx6ajcCsO60RnjwA5MPiYNg20Aupr3KmxVW1OVKLJeZaxcjCW2GzSJtXUks4lS6wQ19cMk7BB5WTVj5mdnJ0_981L6zA5CAHlqgACaXdI9MBuhoTkb0IZEXhHYrbBgMUFUQBPODn9u6epU7d6Vl1ApFVWSka5cK-ck"
+    @user_id = "1270051882"
   end
-  describe 'get requests' do
-    describe '/weather_playlist' do
-      it "returns current weather data for a particular location city, state, country" do
-        VCR.use_cassette('weather_city_state_country') do
+
+      it "returns current weather data for a particular location city, state, country", :vcr do
           location = "denver, co, usa"
           get "/weather_playlist?units=imperial&q=#{location}&token=#{@token}"
 
@@ -41,11 +39,9 @@ describe WeatherMusicController do
 
           expect(weather[:data][:weather][:attributes][:city_name]).to eq ("Denver")
           expect(weather[:data][:weather][:attributes][:country_name]).to eq ("US")
-        end
       end
 
-      it "returns current weather data for a particular location city, country" do
-        VCR.use_cassette('weather_city_country') do
+      it "returns current weather data for a particular location city, country", :vcr do
           location = "santiago, chi"
           get "/weather_playlist?units=imperial&q=#{location}&token=#{@token}"
 
@@ -73,11 +69,9 @@ describe WeatherMusicController do
 
           expect(weather[:data][:weather][:attributes][:city_name]).to eq ("Santiago")
           expect(weather[:data][:weather][:attributes][:country_name]).to eq ("CL")
-        end
       end
 
-      it "returns current weather data for a particular location city" do
-        VCR.use_cassette('weather_city') do
+      it "returns current weather data for a particular location city", :vcr do
           location = "salt lake city"
           get "/weather_playlist?units=imperial&q=#{location}&token=#{@token}"
 
@@ -106,11 +100,9 @@ describe WeatherMusicController do
 
           expect(weather[:data][:weather][:attributes][:city_name]).to eq ("Salt Lake City")
           expect(weather[:data][:weather][:attributes][:country_name]).to eq ("US")
-        end
       end
 
-      it "returns an error message if location cannot be found" do
-        VCR.use_cassette('invalid location input') do
+      it "returns an error message if location cannot be found", :vcr do
           location = "aowfufrbjna"
           get "/weather_playlist?units=imperial&q=#{location}&token=#{@token}"
 
@@ -125,10 +117,9 @@ describe WeatherMusicController do
           expect(error[:data][:attributes][:code]).to eq(404)
           expect(error[:data][:attributes][:message]).to eq("city_not_found")
         end
-      end
-      it "pulls tracks based on genre if user has no saved tracks" do
-        VCR.use_cassette('genre_path_user_tracks') do
-          token = "BQC3liiIFHKDqn15J72uyAp1Xji3gPA_I9SIDFeH60MHsaor91WMJMIDFpDyUuyLw40gfiQxs9LeN4tRt0rBRZcBan4kb3EHUzL-APCvJoBNm_ElQU2t47oxUx72HBWiJ__VNlRC7o5qHOzrKrF23ibEa-UbHrRxXsNPj3F03Mnchs33IEOTNXY2bbhrH2tx8gf9mWIJLF1ffy2zyCJuem82dlKzKg7N-VChZcbT6jw"
+
+      it "pulls tracks based on genre if user has no saved tracks", :vcr do
+          token = "BQBiuYeSi3oRvtDjt_PFPUFLG5o8_dYuUrt4Rx5vh3WaN87liT1VJTW2kIGvz5aKp-gA5m7YvMcOTENsrU2NDkxD6il7a0qUVRujxN6AUcoRPNYh4Y8C99ymVAsUW58u3BNJhL0r7975vhVKluFXtrBUXJOWJz34Z_JFUvRx3Oh-M5P6NNx9NnfXQHQuzRBSFF4oVbVmOEbEr6ylnCTyUhrYiq4mUDyhNDxUMQYWfxA"
           location = "denver"
           get "/weather_playlist?units=imperial&q=#{location}&token=#{token}"
 
@@ -158,13 +149,8 @@ describe WeatherMusicController do
           expect(weather_music[:data][:weather][:attributes][:city_name]).to eq ("Denver")
           expect(weather_music[:data][:weather][:attributes][:country_name]).to eq ("US")
         end
-      end
-    end
-  end
-  describe 'post requests' do
-    describe 'add playlist to library' do
-      it 'adds a playlist to users library' do
-        VCR.use_cassette('created_playlist') do
+
+      it 'adds a playlist to users library', :vcr do
           playlist_name = "party name"
           tracks = "[\"spotify:track:4mYG4iHgfxlPaqfT3BQ0ec\", \"spotify:track:1Bj6YgjjPbEb4jhQ50T8tJ\", \"spotify:track:6qspW4YKycviDFjHBOaqUY\", \"spotify:track:12G1TYIfbpvC0mdFFn4Pbg\", \"spotify:track:6gJdDnF2TzfA1WPMXuCa3x\", \"spotify:track:7sJN693sYKEIEMu7fc5VnJ\", \"spotify:track:2vytyWClpsahqcL6NibSE3\", \"spotify:track:4Yenz5JZZOUiZSeyKY8bDz\", \"spotify:track:3jpaB4JCMidb5XshQtDSGm\", \"spotify:track:7KFThZQCAcj8JXdPRtdrXE\", \"spotify:track:5vWwuFTSoPdyY8rH1mui8W\", \"spotify:track:4o6BgsqLIBViaGVbx5rbRk\", \"spotify:track:24PWKmemCvqfyVXODhoKHW\", \"spotify:track:4O4Z8VFczL8MxIOmqVWc1b\", \"spotify:track:3whRKAOlJ0M3banzcChvQv\", \"spotify:track:4aOjDKk1s5hlsEY5ZhhX3l\", \"spotify:track:5krOROgmf8adn3SJzeKLZy\", \"spotify:track:0qRR9d89hIS0MHRkQ0ejxX\", \"spotify:track:5LxvwujISqiB8vpRYv887S\", \"spotify:track:1GrikfH0jDejDvrxo84n4P\"]"
 
@@ -177,6 +163,3 @@ describe WeatherMusicController do
           expect(response[:data][:playlist][:attributes]).to have_key :external_url
         end
       end
-    end
-  end
-end
